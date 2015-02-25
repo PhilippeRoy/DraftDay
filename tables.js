cf7799d77962402a1e9fab9f09e25e6d06695784
Philippe Roy <philippe.j.roy@gmail.com>
2015-02-26 08:24:40 +1100

    on the tram

--- tables.js
+++ tables.js
@@ -1,30 +1,86 @@
-var testData = [{name: 'Philippe', points:400},{name:'Jobe', points: 50},{name:'Nick', points: 107}];
+//Data
+var testData = [{name: 'Philippe', points:400, position: 'C'},{name:'Jobe', points: 50, position: 'C'},{name:'Nick', points: 107, position: 'C'}];
 
-function Table(tableData) {
+//Utilities
+function addElement(tag, location, before) {
+  var el = document.createElement(tag);
+  //if before ? location.before(el) : location.appendChild(el);
+
+}
+
+//Classes
+function Dashboard(name) {
+  this.name = name;
+}
+
+Dashboard.prototype = {
+
+  createModule : function(){
+
+    var dashboard = '<div id='+this.name+' ></div>';
+    document.body.innerHTML = dashboard;
+
+    return document.getElementById(this.name)
+  },
+
+  createSubModule : function(name){
+
+    var subModule = document.createElement('div');
+    subModule.className = name + ' ' + 'module';
+    document.getElementById(this.name).appendChild(subModule);
+
+    return document.getElementById(this.name).getElementsByClassName(name)[0];
+
+  },
+
+  addTable : function(table){
+     //console.log(table);
+     //need to bind to caller
+     table.init();
+  }
+}
+
+function Table(name) {
+  this.name = name;
 }
 
 Table.prototype = {
 
   init : function(tableData){
     console.log('created');
-    //console.log(tableData === typeof Array);
-    console.log(tableData.length > 0);
-    //if(tableData === typeof Array){
-      if(tableData.length > 0) {
-        var table = '<table>';
-        for(var i = 0; i < tableData.length; i++){
-          table += '<tr><td>';
-          table += tableData[i].name;
-          table += '</td><td>';
-          table += tableData[i].points
-          table += '</td><tr>';
-        }
 
-        table += '</table>';
-        return table;
+    var table = document.createElement('table');
+    //document.getElementById(this.name).appendChild(table);
+
+    //console.log(tableData === typeof Array);
+    //if(tableData === typeof Array){
+      // if(tableData.length > 0) {
+      //   var table = '<table>';
+      //   for(var i = 0; i < tableData.length; i++){
+      //     table += '<tr><td>';
+      //     table += tableData[i].name;
+      //     table += '</td><td>';
+      //     table += tableData[i].points;
+      //     table += '</td><td>';
+      //     table += '<button class="select">selected</button>';
+      //     table += '<button class="delete">delete</button>';
+      //     table += '</td><tr>';
+      //   }
+      //
+      //   table += '</table>';
+      //   return table;
         //add event listeners
+        //on hover - display delete button
     //  }
-    }
+    //}
+
+  },
+
+  addListener : function(){
+
+  },
+
+  removeListener : function(){
 
   },
 
@@ -36,14 +92,32 @@ Table.prototype = {
   removeRow : function(index){
     console.log('removed');
 
+
   }
 
 }
 
 
+var dashboard = new Dashboard('master');
+//Create Dashboard
+var theDashboard = dashboard.createModule();
 
-var globalTable = new Table();
-var backTable = new Table();
+var backsSubModule = dashboard.createSubModule('global');
 
-console.log(backTable.createTable(testData));
-document.getElementsByTagName('body')[0].innerHTML = backTable.init(testData);
+
+var globalTable = new Table('global');
+
+theDashboard.addTable(globalTable);
+
+//globalTable.init(testData);
+globalTable.gatherTablesData = function(){
+  //find all submoles in master
+  //read tables and make array
+  //sort array
+  //output array
+}
+
+
+//var backTable = new Table();
+
+//document.getElementsByTagName('body')[0].innerHTML = backTable.init(testData);
